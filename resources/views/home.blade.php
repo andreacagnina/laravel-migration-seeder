2525b2e7206ae1bae87971b9041bc7f230aa6af2
andreacagnina <amirkal@libero.it>
2024-09-26 18:50:28 +0200

    [feat] add if cond

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,29 +4,29 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1 class="text-center">I treni Ciuff Ciuff 🚊</h1>
+                <h1 class="text-center">I treni Ciuff Ciuff🚊in partenza oggi</h1>
                 <div class="row">
                     <div class="col-12">
                         @foreach ($trains as $train)
                             <ul>
-                                <li>
-                                    <strong>Company:</strong> {{ $train->company }}<br>
-                                    <strong>Stazione di Partenza:</strong> {{ $train->departure_station }}<br>
-                                    <strong>Stazione di arrivo:</strong> {{ $train->arrival_station }}<br>
-                                    <strong>Orario di partenza:</strong> {{ $train->departure_time }}<br>
-                                    <strong>Orario di arrivo:</strong> {{ $train->arrival_time }}<br>
-                                    <strong>Codice vettura:</strong> {{ $train->train_code }}<br>
-                                    <strong>Numero carrozze:</strong> {{ $train->number_of_carriages }}<br>
-                                    @if ($train->delay)
-                                        <strong>IL TRENO è IN RITARDO, CI SCUSIAMO PER IL DISAGIO</strong><br>
-                                    @endif
+                                @if ($train->departure_station != $train->arrival_station && $train->arrival_time > $train->departure_time)
+                                    <li>
+                                        <strong>Company:</strong> {{ $train->company }}<br>
+                                        <strong>Stazione di Partenza:</strong> {{ $train->departure_station }}<br>
+                                        <strong>Stazione di arrivo:</strong> {{ $train->arrival_station }}<br>
+                                        <strong>Orario di partenza:</strong> {{ $train->departure_time }}<br>
+                                        <strong>Orario di arrivo:</strong> {{ $train->arrival_time }}<br>
+                                        <strong>Codice vettura:</strong> {{ $train->train_code }}<br>
+                                        <strong>Numero carrozze:</strong> {{ $train->number_of_carriages }}<br>
+                                        @if ($train->delay)
+                                            <strong>IL TRENO è IN RITARDO, CI SCUSIAMO PER IL DISAGIO</strong><br>
+                                        @endif
 
-                                    @if ($train->suppressed)
-                                        <strong>IL TRENO è STATO SOPPRESSO</strong>
-                                    @endif
-
-
-                                </li>
+                                        @if ($train->suppressed)
+                                            <strong>IL TRENO è STATO SOPPRESSO</strong>
+                                        @endif
+                                    </li>
+                                @endif
                             </ul>
                         @endforeach
                     </div>
